c9756ec13b162d589a3208e3686ac707b1a6cfd7
Jai Suriya <85334558+jairoyy@users.noreply.github.com>
2023-02-02 10:43:06 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -10,3 +10,16 @@ prompt("What is your name?");
 
 var myName="Jairoy";
 alert(myName);
+
+
+
+(function () {
+  "use strict";
+  /* Start of your code */
+  function greetMe(yourName) {
+    alert(`Hello ${yourName}`);
+  }
+
+  greetMe("World");
+  /* End of your code */
+})();
