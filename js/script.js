5e776f0b896d64e83907700e18c69bd78cd25f00
Jai Suriya <85334558+jairoyy@users.noreply.github.com>
2023-02-03 18:29:13 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -23,3 +23,19 @@ alert(myName);
   greetMe("World");
   /* End of your code */
 })();
+
+
+
+
+
+var x = 1;
+
+if (x === 1) {
+  var x = 2;
+
+  console.log(x);
+  // Expected output: 2
+}
+
+console.log(x);
+// Expected output: 2
